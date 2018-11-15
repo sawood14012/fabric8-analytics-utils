82fc9c25fcc36696bfa2359f7f3f22c93c296289
Michal Srb <michal@redhat.com>
2018-11-15 18:10:23 +0100

    Silence unused-import error

--- f8a_utils/commands/__init__.py
+++ f8a_utils/commands/__init__.py
@@ -1,3 +1,7 @@
 """Commands package."""
 
 from f8a_utils.commands.command import ExternalCommand
+
+
+# Silence linters...
+assert ExternalCommand is not None
--- whitelist.py
+++
@@ -1,1 +0,0 @@
-ExternalCommand  # unused import (f8a_utils/commands/__init__.py:3)
