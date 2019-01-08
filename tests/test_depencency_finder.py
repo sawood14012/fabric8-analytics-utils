2c30065eddf077af35cfe076b1fcacb0cb036bed
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-08 09:38:21 +0100

    Test for manifests represented as array of bytes

--- tests/test_depencency_finder.py
+++ tests/test_depencency_finder.py
@@ -4,12 +4,25 @@ from f8a_utils.dependency_finder import DependencyFinder
 from pathlib import Path
 
 
-def test_scan_and_find_dependencies():
+def test_scan_and_find_dependencies_npm():
     """Test scan_and_find_dependencies function."""
     manifests = [{
         "filename": "npmlist.json",
         "filepath": "/bin/local",
         "content": open(str(Path(__file__).parent / "data/npmlist.json")).read()
+    }]
+    res = DependencyFinder().scan_and_find_dependencies("npm", manifests)
+    assert "result" in res
+    assert res['result'][0]['details'][0]['_resolved'][0]['package'] == "body-parser"
+    assert len(res['result'][0]['details'][0]['_resolved'][0]['deps']) == 2
+
+
+def test_scan_and_find_dependencies_npm_npm_list_as_bytes():
+    """Test scan_and_find_dependencies function."""
+    manifests = [{
+        "filename": "npmlist.json",
+        "filepath": "/bin/local",
+        "content": open(str(Path(__file__).parent / "data/npmlist.json"), "rb").read()
     }]
     res = DependencyFinder().scan_and_find_dependencies("npm", manifests)
     assert "result" in res
@@ -23,6 +36,19 @@ def test_scan_and_find_dependencies_pypi():
         "filename": "pylist.json",
         "filepath": "/bin/local",
         "content": open(str(Path(__file__).parent / "data/pylist.json")).read()
+    }]
+    res = DependencyFinder().scan_and_find_dependencies("pypi", manifests)
+    assert "result" in res
+    assert res['result'][0]['details'][0]['_resolved'][0]['package'] == "django"
+    assert len(res['result'][0]['details'][0]['_resolved'][0]['deps']) == 1
+
+
+def test_scan_and_find_dependencies_pypi_pylist_as_bytes():
+    """Test scan_and_find_dependencies function for pypi."""
+    manifests = [{
+        "filename": "pylist.json",
+        "filepath": "/bin/local",
+        "content": open(str(Path(__file__).parent / "data/pylist.json"), "rb").read()
     }]
     res = DependencyFinder().scan_and_find_dependencies("pypi", manifests)
     assert "result" in res
@@ -44,7 +70,25 @@ def test_scan_and_find_dependencies_maven():
     assert len(resolved['deps']) == 15
 
 
+def test_scan_and_find_dependencies_maven_manifest_as_bytes():
+    """Test scan_and_find_dependencies function for maven."""
+    # file containing content should be opened as binary stream
+    manifests = [{
+        "filename": "dependencies.txt",
+        "filepath": "/bin/local",
+        "content": open(str(Path(__file__).parent / "data/dependencies.txt"), "rb").read()
+    }]
+    res = DependencyFinder().scan_and_find_dependencies("maven", manifests)
+    assert "result" in res
+    resolved = res['result'][0]['details'][0]['_resolved'][0]
+    assert resolved['package'] == "io.vertx:vertx-core"
+    assert len(resolved['deps']) == 15
+
+
 if __name__ == '__main__':
-    test_scan_and_find_dependencies()
+    test_scan_and_find_dependencies_npm()
+    test_scan_and_find_dependencies_npm_npm_list_as_bytes()
     test_scan_and_find_dependencies_pypi()
+    test_scan_and_find_dependencies_pypi_pylist_as_bytes()
     test_scan_and_find_dependencies_maven()
+    test_scan_and_find_dependencies_maven_manifest_as_bytes()
