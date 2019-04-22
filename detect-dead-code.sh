51d260db511dd697a289a30975efbcd054c33f52
Yusuf Zainee <yzainee@localhost.localdomain>
2019-04-22 19:51:12 +0530

    python version change

--- detect-dead-code.sh
+++ detect-dead-code.sh
@@ -7,10 +7,10 @@ pass=0
 fail=0
 
 function prepare_venv() {
-    VIRTUALENV=$(which virtualenv)
+    VIRTUALENV="$(which virtualenv)"
     if [ $? -eq 1 ]; then
         # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV=$(which virtualenv-3)
+        VIRTUALENV="$(which virtualenv-3)"
     fi
 
     if [ $? -eq 1 ]; then
